6b5f1eef837faae599b86af4b2a00f011e4ac131
Geonhee Son <ryan.son1002@gmail.com>
2022-09-19 02:05:44 +0900

    feat: `HikeView` `showDetail`에 따른 애니메이션 및 트랜지션 추가

--- Landmarks/Sources/Hikes/HikeView.swift
+++ Landmarks/Sources/Hikes/HikeView.swift
@@ -7,6 +7,15 @@
 
 import SwiftUI
 
+extension AnyTransition {
+  static var moveAndFade: AnyTransition {
+    return .asymmetric(
+      insertion: .move(edge: .trailing).combined(with: .opacity),
+      removal: .scale.combined(with: .opacity)
+    )
+  }
+}
+
 struct HikeView: View {
   var hike: Hike
   @State private var showDetail = false
@@ -25,17 +34,26 @@ struct HikeView: View {
 
         Spacer()
 
-        Button(action: { showDetail.toggle() }) {
-          Label("Graph", systemImage: "chevron.right.circle")
-            .labelStyle(.iconOnly)
-            .imageScale(.large)
-            .rotationEffect(.degrees(showDetail ? 90 : 0))
-            .padding()
-        }
+        Button(
+          action: {
+            withAnimation {
+              showDetail.toggle()
+            }
+          },
+          label: {
+            Label("Graph", systemImage: "chevron.right.circle")
+              .labelStyle(.iconOnly)
+              .imageScale(.large)
+              .rotationEffect(.degrees(showDetail ? 90 : 0))
+              .scaleEffect(showDetail ? 1.5 : 1)
+              .padding()
+          }
+        )
       }
 
       if showDetail {
         HikeDetail(hike: hike)
+          .transition(.moveAndFade)
       }
     }
   }
