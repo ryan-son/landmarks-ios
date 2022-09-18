dae856aad40b554c88747133d1c5038413ffe0e5
Geonhee Son <ryan.son1002@gmail.com>
2022-09-18 20:23:02 +0900

    feat: preview를 위해 각 View에 작성한 하드코딩 요소 제거

--- Landmarks/Sources/LandmarkList/LandmarkList.swift
+++ Landmarks/Sources/LandmarkList/LandmarkList.swift
@@ -12,9 +12,10 @@ struct LandmarkList: View {
   var body: some View {
     NavigationView {
       List(landmarks) { landmark in
-        NavigationLink(destination: LandmarkDetail.init) {
-          LandmarkRow(landmark: landmark)
-        }
+        NavigationLink(
+          destination: { LandmarkDetail(landmark: landmark) },
+          label: { LandmarkRow(landmark: landmark) }
+        )
       }
       .navigationTitle("Landmarks")
     }
