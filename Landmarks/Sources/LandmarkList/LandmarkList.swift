b76b7043a25d68fa99a0a6dc9b01a26767875521
Geonhee Son <ryan.son1002@gmail.com>
2022-09-18 20:11:05 +0900

    feat: `LandmarkList` 추가

---
+++ Landmarks/Sources/LandmarkList/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Geonhee on 2022/09/18.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+
+  var body: some View {
+    List(landmarks) { landmark in
+      LandmarkRow(landmark: landmark)
+    }
+  }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+  static var previews: some View {
+    LandmarkList()
+  }
+}
