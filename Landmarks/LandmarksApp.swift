b978581469c4973230fdea83c93c2f2a9ead1c26
Geonhee Son <ryan.son1002@gmail.com>
2022-09-18 17:44:35 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Geonhee on 2022/09/18.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
