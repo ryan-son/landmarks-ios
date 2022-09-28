3888257064308b9f9131d387a76cfa3ed687dce2
Geonhee Son <ryan.son1002@gmail.com>
2022-09-28 21:01:49 +0900

    feat: `ProfileHost` `EditMode` 추가

--- Landmarks/Sources/Profile/ProfileHost.swift
+++ Landmarks/Sources/Profile/ProfileHost.swift
@@ -8,11 +8,22 @@
 import SwiftUI
 
 struct ProfileHost: View {
+  @Environment(\.editMode) var editMode
+  @EnvironmentObject var modelData: ModelData
   @State private var draftProfile = Profile.default
 
   var body: some View {
     VStack(alignment: .leading, spacing: 20) {
-      ProfileSummary(profile: draftProfile)
+      HStack {
+        Spacer()
+        EditButton()
+      }
+
+      if editMode?.wrappedValue == .inactive {
+        ProfileSummary(profile: modelData.profile)
+      } else {
+        Text("Profile Editor")
+      }
     }
     .padding()
   }
